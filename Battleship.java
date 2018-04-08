605f610122ae7bbae1e76632cb7b1957636034c3
swillits3 <swillits3@live.com>
2018-04-08 02:50:37 -0400

    Updated menu

--- Battleship.java
+++ Battleship.java
@@ -10,20 +10,23 @@ public class Battleship
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame( "Battleship" );
+		frame.getContentPane().setLayout(null);
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.setSize( 900, 615 ); // set frame size
+		frame.setResizable(false);
+
+		//code this in if setSize() doesn't work
+		//frame.setPreferredSize(new Dimension(900, 615));
+		//frame.setMinimumSize(new Dimension(900, 615));		
+		//frame.pack();
+		//frame.setVisible( true ); // display frame
+
 
 		
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().setBackground(Color.BLACK);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//opens JFrame for other operations
-		frame.setPreferredSize(new Dimension(900, 615));
-		frame.setMinimumSize(new Dimension(900, 615));
-		frame.setResizable(false);
-		frame.pack();
-		
 		Menu menu = new Menu(frame); 
-		frame.add( menu );
-		frame.setSize( 900, 650 ); // set frame size
-		frame.setVisible( true ); // display frame
+		menu.run();
+		while(menu.running())	//be on menu until user presses play
+		{}
+
 	}
 }
--- Menu.java
+++ Menu.java
@@ -1,28 +1,53 @@
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
 
 
-public class Menu extends JPanel
+public class Menu
 {
 	public JFrame menu;
-	public ImageIcon picture;
-	public JLabel pictureholder;
-	public JButton start;
+	public JButton start,load;
+	public JLabel picture;
+
+	public boolean running;
 	
 	public Menu (JFrame frame)
-	{
+	{	
 		menu = frame;
-		pictureholder= new JLabel(picture);
-		picture = new ImageIcon("Title.png");
+		picture= new JLabel(new ImageIcon("Title.png"));
+	}
+	
+	public void run()
+	{
+		//for menu picture
+		picture.setSize(menu.getWidth(),menu.getHeight());
+		picture.setLocation(0,0);
+		picture.setVisible(true);
 		
+		//for start button
+		start=new JButton("START");
+		//start.setBackground(Color.BLACK);
+		start.setSize(300,125);
+		start.setLocation(menu.getWidth()/2,menu.getHeight()-230);
+		start.setVisible(true);
+		
+		//for load button
+		
+		//at the end, add everything to frame and set visible
+		menu.getContentPane().add(picture);
+		menu.getContentPane().add(start);
+		
+		running=true;
 		menu.setVisible(true);
+		
+		menu.getContentPane().revalidate();
+		menu.getContentPane().repaint();
+	}
+	
+	public boolean running()
+	{
+		return running;
 	}
 }	
