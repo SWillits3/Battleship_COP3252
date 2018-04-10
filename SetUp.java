76640009a97f220b18b5ec0d1a8be8a11c5d9abe
swillits3 <swillits3@live.com>
2018-04-09 21:09:39 -0400

    made grid fit on panel

--- SetUp.java
+++ SetUp.java
@@ -4,6 +4,10 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.*;
 
 import javax.swing.*;
 
@@ -11,17 +15,30 @@ public class SetUp extends JPanel
 {
 	private JFrame window; 
 	private boolean running;
+	private Image pic;
+	private JLabel picture;
 	
 	public SetUp (JFrame frame)
 	{
 	   window = frame;
 	   running =true;
+	   Image temppic;
+	   try
+	   {
+		   pic = ImageIO.read(new File("grid.png"));		//resizing grid picture
+	   }
+	   catch (IOException e)
+	   {
+		   
+	   }
+	   int size=window.getContentPane().getHeight();
+	   pic = pic.getScaledInstance(size,size,Image.SCALE_DEFAULT);
 	}
 	
 	public void run()
 	{
-		JLabel picture= new JLabel(new ImageIcon("Grid.png"));
-		picture.setSize(window.getWidth(),window.getHeight());
+		picture= new JLabel(new ImageIcon(pic));
+		picture.setSize(window.getContentPane().getHeight(),window.getContentPane().getHeight());
 		picture.setLocation(0,0);
 		picture.setVisible(true);
 		window.add(picture);
